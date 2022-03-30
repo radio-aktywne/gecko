5730e3439532f9b75770401809c92e31c80b05e0
Sebastian Pietras <spietras1998@gmail.com>
2022-03-30 22:51:29 +0200

    Added casting port to int (#4)

--- emirecorder/src/emirecorder/config.py
+++ emirecorder/src/emirecorder/config.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    port: int = os.getenv("EMIRECORDER_PORT", 31000)
+    port: int = int(os.getenv("EMIRECORDER_PORT", 31000))
     target_host: str = os.getenv("EMIRECORDER_TARGET_HOST", "localhost")
-    target_port: int = os.getenv("EMIRECORDER_TARGET_PORT", 30000)
+    target_port: int = int(os.getenv("EMIRECORDER_TARGET_PORT", 30000))
     target_user: str = os.getenv("EMIRECORDER_TARGET_USER", "readwrite")
     target_password: str = os.getenv("EMIRECORDER_TARGET_PASSWORD", "password")
 
