5b7383e1743cef89414490dae9b836975422058b
Sebastian Pietras <spietras1998@gmail.com>
2022-10-19 07:50:15 +0200

    Updated files to match project template

--- emirecorder/src/emirecorder/app.py
+++ emirecorder/src/emirecorder/app.py
@@ -1,17 +1,17 @@
 from starlite import Starlite, State
 
+from emirecorder.config import Config
 from emirecorder.paths.router import router
 from emirecorder.stream.management import StreamManager
 
 
-async def setup(state: State) -> None:
-    state.stream_manager = StreamManager()
+def build_app(config: Config) -> Starlite:
+    async def setup(state: State) -> None:
+        state.stream_manager = StreamManager(config)
 
+    async def cleanup(state: State) -> None:
+        pass
 
-async def cleanup(state: State) -> None:
-    pass
-
-
-app = Starlite(
-    route_handlers=[router], on_startup=[setup], on_shutdown=[cleanup]
-)
+    return Starlite(
+        route_handlers=[router], on_startup=[setup], on_shutdown=[cleanup]
+    )
