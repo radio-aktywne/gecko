098e3db570f64095d6c725d3dde5f6a90bfef08b
Sebastian Pietras <me@spietras.dev>
2023-12-20 22:09:00 +0000

    Fetched template changes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,12 @@
 import pytest
+import pytest_asyncio
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
 
 from emirecorder.api.app import AppBuilder
 from emirecorder.config.builder import ConfigBuilder
 from emirecorder.config.models import Config
+from tests.utils.containers import AsyncDockerContainer, ContainerWaiter
 
 
 @pytest.fixture(scope="session")
@@ -21,8 +23,38 @@ def app(config: Config) -> Litestar:
     return AppBuilder(config).build()
 
 
-@pytest.fixture(scope="session")
-def client(app: Litestar) -> AsyncTestClient:
+@pytest_asyncio.fixture(scope="session")
+async def emiarchive(config: Config) -> AsyncDockerContainer:
+    """Emiarchive container."""
+
+    container = AsyncDockerContainer(
+        "ghcr.io/radio-aktywne/apps/emiarchive:latest",
+        network="host",
+    )
+
+    async with container as container:
+        # Wait for emiarchive to start.
+        waiter = ContainerWaiter(
+            container,
+            [
+                "./scripts/shell.sh",
+                "mc",
+                "config",
+                "host",
+                "add",
+                "minio",
+                f"http://localhost:{config.emiarchive.port}",
+                config.emiarchive.user,
+                config.emiarchive.password,
+            ],
+        )
+        await waiter.wait()
+        yield container
+
+
+@pytest_asyncio.fixture(scope="session")
+async def client(app: Litestar, emiarchive: AsyncDockerContainer) -> AsyncTestClient:
     """Reusable test client."""
 
-    return AsyncTestClient(app=app)
+    async with AsyncTestClient(app=app) as client:
+        yield client
