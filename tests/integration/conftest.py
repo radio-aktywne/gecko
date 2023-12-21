9a1cee42578f7b58de6178afbca9e13012a26ed0
Sebastian Pietras <me@spietras.dev>
2023-12-21 19:41:08 +0000

    Fetched template changes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,12 +1,16 @@
 import pytest
 import pytest_asyncio
+from httpx import AsyncClient
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
 
 from emirecorder.api.app import AppBuilder
 from emirecorder.config.builder import ConfigBuilder
 from emirecorder.config.models import Config
-from tests.utils.containers import AsyncDockerContainer, ContainerWaiter
+from tests.utils.containers import AsyncDockerContainer
+from tests.utils.waiting.conditions import CallableCondition
+from tests.utils.waiting.strategies import TimeoutStrategy
+from tests.utils.waiting.waiter import Waiter
 
 
 @pytest.fixture(scope="session")
@@ -24,30 +28,25 @@ def app(config: Config) -> Litestar:
 
 
 @pytest_asyncio.fixture(scope="session")
-async def emiarchive(config: Config) -> AsyncDockerContainer:
+async def emiarchive() -> AsyncDockerContainer:
     """Emiarchive container."""
+
+    async def _check() -> None:
+        async with AsyncClient(base_url="http://localhost:30000") as client:
+            response = await client.get("/minio/health/ready")
+            response.raise_for_status()
 
     container = AsyncDockerContainer(
         "ghcr.io/radio-aktywne/apps/emiarchive:latest",
         network="host",
     )
 
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
     async with container as container:
-        # Wait for emiarchive to start.
-        waiter = ContainerWaiter(
-            container,
-            [
-                "./scripts/shell.sh",
-                "mc",
-                "config",
-                "host",
-                "add",
-                "minio",
-                f"http://localhost:{config.emiarchive.port}",
-                config.emiarchive.user,
-                config.emiarchive.password,
-            ],
-        )
         await waiter.wait()
         yield container
 
